ab29e5ba96efe70c0ddda4a49eabe711b79dc2f5
andrescruz01 <cruz_andres@outlook.com>
2025-03-15 16:55:34 +0100

    More Changes
    Created a new dataframe, and made an initial analysis on the specific dataframe

--- Code/school_data_analysis.R
+++ Code/school_data_analysis.R
@@ -92,6 +92,199 @@ my_theme <- theme(
 #
 #
 
+###################### ---- School Data Betyg ---- ######################
+
+#
+#
+
+# -- Data Preparation -- #
+
+# Reading school data excel file and importing english sheet
+school_data_betyg <- read_excel("school_data_betyg.xlsx", sheet = "Betyg")
+
+# Filter 
+school_data_betyg <- school_data_betyg %>%
+  filter(school_size != '1-49') 
+
+# %>% filter(academic_year != "2021/22") 
+
+# Variable Modification
+school_data_betyg <- school_data_betyg %>%
+  mutate(treatment_year = ifelse(academic_year == "2019/20", 1, 0), 
+         treatment_group = ifelse(educational_stage == "gymnasieskola", 1, 0),
+         DiD = treatment_year*treatment_group,
+         private_ownership = ifelse(type_of_ownership == "Enskild", 1, 0),
+         school_ID = as.factor(school_ID),
+         academic_year = as.factor(academic_year),
+         school_municipality = as.factor(school_municipality),
+         school_county = as.factor(school_municipality),
+         academic_year_spring = as.numeric(paste0("20", sub(".*/", "", academic_year)))
+  )
+
+# Converting to pandel data
+school_data_betyg <- pdata.frame(school_data_betyg, index = c("school_ID", "academic_year"))
+
+# Define the path to the output folder
+path_output_betyg <- "/Users/andrescruz/Documents/Handelshögskolan/MSc Economic/Semester 4/5350 Thesis in Economics/Output/Betyg/"
+
+#
+#
+
+# -- Parallel Trend Assumption -- #
+
+#
+#
+
+# - Parallel Trend Plot - #
+
+# Time Trend
+time_trend <- school_data_betyg %>% 
+  drop_na() %>% 
+  group_by(academic_year, educational_stage) %>% 
+  summarise(average_grade_points = mean(average_grade_points))
+
+# Plot the Time Trend for each educational stage
+parallel_trend_plot <- ggplot(time_trend, 
+                              mapping=aes(x=academic_year, y=average_grade_points, 
+                                          group=educational_stage, color=educational_stage, shape=educational_stage)) + 
+  # Adding dots
+  geom_point(size = 2.5) +
+  # Custom color and name 
+  scale_color_manual(values = c('grundskola' = 'darkgrey', 'gymnasieskola' = 'black'),
+                     labels = c('grundskola' = 'Lower Secondary, Grade 9', 'gymnasieskola' = 'Upper Secondary, Grade 10')) + 
+  # Custom shapes
+  scale_shape_manual(values = c('grundskola' = 20, 'gymnasieskola' = 18),  
+                     labels = c('grundskola' = 'Lower Secondary, Grade 9', 'gymnasieskola' = 'Upper Secondary, Grade 10')) +
+  # Connecting dots
+  geom_line(size = 0.5) +
+  # X intercept
+  geom_vline(xintercept = 3.5, linetype = "dashed") + 
+  # Modify text
+  labs(
+    title = "Parallel Trend Assumption - Betyg",
+    x = "Academic Year",
+    y = "Average percent of students with F"
+  ) +   
+  ylim(12, 15) +
+  # My theme
+  my_theme
+
+# Printing Plot
+print(parallel_trend_plot)
+
+# Use file.path() to ensure correct formatting of the file path
+output_file_betyg <- file.path(path_output_betyg, "parallel_trend_plot.png")
+
+# Save the plot
+ggsave(output_file_betyg, parallel_trend_plot, bg = "transparent", width=6, height=3)
+
+#
+#
+#
+#
+
+# - Dynamic Difference-in-Difference - #
+
+# Setting the reference year
+school_data_betyg$academic_year <- relevel(school_data_betyg$academic_year, ref="2018/19")
+
+# Estimating the Dynamic DiD Model
+dynamic_DiD <- plm(
+  average_grade_points ~ 
+    academic_year*(treatment_group + share_foreign_background + share_postsecondary_parents + 
+                     share_active_certified_teachers + private_ownership + share_female_students), 
+  data = school_data_betyg, model = "within", effect = "individual")
+
+dynamic_DiD_summary <- summary(dynamic_DiD) # Storing the results
+
+## with plm, slightly different
+dynamic_DiD_clustered_se <- sqrt(diag(plm::vcovHC(dynamic_DiD, type = "HC1", cluster="group")))
+
+
+# Order of Coefficients for the event study plot
+plot_order <- c('academic_year2016/17:treatment_group', 
+                'academic_year2017/18:treatment_group', 
+                'academic_year2019/20:treatment_group',
+                'academic_year2020/21:treatment_group', 
+                'academic_year2021/22:treatment_group') 
+
+# Extracting Coefficients and Using Clustered Standard Errors
+dynamic_DiD_results_clustered <- tibble(
+  estimates = c(dynamic_DiD_summary$coefficients[plot_order, "Estimate"], 0),
+  standard_errors = c(dynamic_DiD_clustered_se[plot_order], 0),  # Use clustered SEs
+  label = c(-3, -2, 0, 1, 2, -1)
+)
+
+dynamic_did_plot <- ggplot(data = dynamic_DiD_results_clustered,  
+                           aes(x = label, y = estimates)) +
+  geom_errorbar(aes(ymin = estimates - 1.96 * standard_errors, 
+                    ymax = estimates + 1.96 * standard_errors), 
+                size = 0.5, width = 0.05, color = "black", alpha = 0.75) +  
+  geom_point(shape = 18, size = 2.5, color = "black") + 
+  xlab('Years before and after school closures') +
+  ylab('Average Grade Points') +
+  geom_hline(yintercept = 0, linetype = "solid", color = 'brown', alpha = 0.75) +  
+  geom_vline(xintercept = -0.5, linetype = "dashed") + 
+  ylim(-0.6, 0.6) + 
+  my_theme
+
+# Print Dynamic DiD Plot
+print(dynamic_did_plot)
+
+# Use file.path() to ensure correct formatting of the file path
+output_file_betyg <- file.path(path_output_betyg, "dynamic_did_plot.png")
+
+# Save the plot
+ggsave(output_file_betyg, dynamic_did_plot, bg = "transparent", width=6, height=3)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+
 ###################### ---- School Data English ---- ######################
 
 #
@@ -100,7 +293,7 @@ my_theme <- theme(
 # -- Data Preparation -- #
 
 # Reading school data excel file and importing english sheet
-school_data_english <- read_excel("school_data.xlsx", sheet = "English")
+school_data_english <- read_excel("school_data_ämnen.xlsx", sheet = "English")
 
 # Filter 
 school_data_english <- school_data_english %>%
@@ -113,11 +306,16 @@ school_data_english <- school_data_english %>%
          treatment_group = ifelse(educational_stage == "gymnasieskola", 1, 0),
          DiD = treatment_year*treatment_group,
          private_ownership = ifelse(type_of_ownership == "Enskild", 1, 0),
+         school_ID = as.factor(school_ID),
          academic_year = as.factor(academic_year),
          school_municipality = as.factor(school_municipality),
          school_county = as.factor(school_municipality),
          academic_year_spring = as.numeric(paste0("20", sub(".*/", "", academic_year)))
   )
+
+# Converting to pandel data
+school_data_english <- pdata.frame(school_data_english, index = c("school_ID", "academic_year"))
+
 
 # Define the path to the output folder
 path_output_english <- "/Users/andrescruz/Documents/Handelshögskolan/MSc Economic/Semester 4/5350 Thesis in Economics/Output/English/"
@@ -160,6 +358,7 @@ parallel_trend_plot <- ggplot(time_trend,
     x = "Academic Year",
     y = "Average percent of students with F"
   ) +   
+  ylim(0, 12) +
   # My theme
   my_theme
 
@@ -170,112 +369,45 @@ print(parallel_trend_plot)
 output_file_english <- file.path(path_output_english, "parallel_trend_plot.png")
 
 # Save the plot
-ggsave(output_file_english, parallel_trend_plot, bg = "transparent", width=5, height=4)
+ggsave(output_file_english, parallel_trend_plot, bg = "transparent", width=6, height=3)
 
 #
 #
 #
-
-
-
-
-
-
-
-
+#
 
 # - Dynamic Difference-in-Difference - #
 
-# Set the reference (baseline) category to 2018/19 following Björkegren, Svaleryd and Vlachos (2024)
+# Setting the reference year
 school_data_english$academic_year <- relevel(school_data_english$academic_year, ref="2018/19")
 
-# Converting to to panel data
-# school_data_panel <- pdata.frame(school_data_english, index = c("school_ID", "academic_year"))
-
-# Check if the panel is balanced
-# is.pbalanced(school_data_panel)
-
-# Discrete Academic Year Variable
-event_study_data <- school_data_english %>% 
-  mutate(academic_year_spring = as.numeric(paste0("20", sub(".*/", "", academic_year)))) %>% 
-  mutate(
-    time_from_treatment = academic_year_spring - 2020, # MAGIC NUMBER CORRESPONDS TO THE TREATMENT YEAR
-    lead_1 = case_when(time_from_treatment == -1 ~ 1, TRUE ~ 0),
-    lead_2 = case_when(time_from_treatment == -2 ~ 1, TRUE ~ 0),
-    lead_3 = case_when(time_from_treatment == -3 ~ 1, TRUE ~ 0),
-    
-    lag_0 = case_when(time_from_treatment == 0 ~ 1, TRUE ~ 0))
-
-
-# Run Dynamic DiD Fixed Effects Model
-lm_test <- lm(share_students_F_eng ~ (lead_3 + lead_2 + lead_1 + lag_0)*treatment_group, data = event_study_data)
-summary(lm_test)
-
-# Another test
-test_2 <- lm(share_students_F_eng ~ 
-               academic_year*treatment_group + share_foreign_background + share_postsecondary_parents + 
-               share_active_certified_teachers + private_ownership + share_female_students, 
-             data = event_study_data)
-
-summary(test_2) # Storing the results
-
-## with plm, slightly different
-rob_cov_mat_plm <- plm::vcovHC(plm_elast_TWFE, type = "HC1", cluster="time")
-robust_se_plm <- sqrt(diag(rob_cov_mat_plm))
-robust_se_plm
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 # Estimating the Dynamic DiD Model
-dynamic_DiD <- lm(share_students_F_eng ~ academic_year*(treatment_group + share_foreign_background + share_postsecondary_parents + 
-                                                          share_active_certified_teachers + private_ownership + share_female_students), 
-                  data = school_data_english)
+dynamic_DiD <- plm(
+  share_students_F_eng ~ 
+    academic_year*(treatment_group + share_foreign_background + share_postsecondary_parents + 
+                     share_active_certified_teachers + private_ownership + share_female_students), 
+  data = school_data_english, model = "within", effect = "individual")
 
 dynamic_DiD_summary <- summary(dynamic_DiD) # Storing the results
 
-dynamic_DiD_clustered_se <- sqrt(diag(vcovCL(dynamic_DiD, cluster = ~ school_municipality))) # Clustered Standard Errors
+## with plm, slightly different
+robust_se_plm <- sqrt(diag(plm::vcovHC(dynamic_DiD, type = "HC1", cluster="group")))
+robust_se_plm
+
+cl_se_plm <- sqrt(diag(vcovHC(dynamic_DiD, type = "HC1",cluster="group")))
+cl_se_plm
+
 
 # Order of Coefficients for the event study plot
 plot_order <- c('academic_year2016/17:treatment_group', 
                 'academic_year2017/18:treatment_group', 
-                'academic_year2019/20:treatment_group',
-                'academic_year2020/21:treatment_group')
+                'academic_year2019/20:treatment_group')
 
 # Extracting Coefficients and Using Clustered Standard Errors
 dynamic_DiD_results_clustered <- tibble(
   estimates = c(dynamic_DiD_summary$coefficients[plot_order, "Estimate"], 0),
   standard_errors = c(dynamic_DiD_clustered_se[plot_order], 0),  # Use clustered SEs
-  label = c(-3, -2, 0, 1, -1)
+  label = c(-3, -2, 0, -1)
 )
 
 dynamic_did_plot <- ggplot(data = dynamic_DiD_results_clustered,  
@@ -288,23 +420,17 @@ dynamic_did_plot <- ggplot(data = dynamic_DiD_results_clustered,
   ylab('Average percent of students with F') +
   geom_hline(yintercept = 0, linetype = "solid", color = 'brown', alpha = 0.75) +  
   geom_vline(xintercept = -0.5, linetype = "dashed") +
+  ylim(-0.6, 0.6) + 
   my_theme
 
 # Print Dynamic DiD Plot
 print(dynamic_did_plot)
 
-
-
-#
-#
-#
-#
-
 # Use file.path() to ensure correct formatting of the file path
 output_file_swedish <- file.path(path_output_english, "dynamic_did_plot.png")
 
 # Save the plot
-ggsave(output_file_swedish, dynamic_did_plot, bg = "transparent", width=5, height=4)
+ggsave(output_file_swedish, dynamic_did_plot, bg = "transparent", width=6, height=3)
 
 #
 #
@@ -396,25 +522,6 @@ stargazer::stargazer(
   out = output_file_english  # Save the table as an HTML file
 )
 
-
-library(did)
-period <- 2020
-
-# estimate group-group time average treatment effects
-did.att.gt <- att_gt(yname="share_students_F_eng",
-                     tname="academic_year_spring",
-                     idnam="school_ID",
-                     gname="period",
-                     data=school_data_english,
-                     bstrap=FALSE,
-                     cband=FALSE
-                     )
-summary(did.att.gt)
-
-#> 
-
-
-
 #
 #
 #
@@ -452,7 +559,7 @@ summary(did.att.gt)
 # -- Data Preparation -- #
 
 # Reading school data excel file and importing english sheet
-school_data_mathematics <- read_excel("school_data.xlsx", sheet = "Mathematics")
+school_data_mathematics <- read_excel("school_data_ämnen.xlsx", sheet = "Mathematics")
 
 # Filter 
 school_data_mathematics <- school_data_mathematics %>%
@@ -562,6 +669,7 @@ dynamic_did_plot <- ggplot(data = dynamic_DiD_results_clustered,
   ylab('Average percent of students with F') +
   geom_hline(yintercept = 0, linetype = "solid", color = 'brown', alpha = 0.75) +  
   geom_vline(xintercept = -0.5, linetype = "dashed") +
+  ylim(-6,6) + 
   my_theme
 
 # Print Dynamic DiD Plot
@@ -571,7 +679,7 @@ print(dynamic_did_plot)
 output_file_mathematics <- file.path(path_output_mathematics, "dynamic_did_plot.png")
 
 # Save the plot
-ggsave(output_file_mathematics, dynamic_did_plot, bg = "transparent", width=5, height=4)
+ggsave(output_file_mathematics, dynamic_did_plot, bg = "transparent", width=6, height=3)
 
 
 #
@@ -701,7 +809,7 @@ stargazer::stargazer(
 # -- Data Preparation -- #
 
 # Reading school data excel file and importing english sheet
-school_data_swedish <- read_excel("school_data.xlsx", sheet = "Swedish")
+school_data_swedish <- read_excel("school_data_ämnen.xlsx", sheet = "Swedish")
 
 # Filter 
 school_data_swedish <- school_data_swedish %>%
@@ -758,7 +866,7 @@ parallel_trend_plot <- ggplot(time_trend,
     x = "Academic Year",
     y = "Average percent of students with F"
   ) +   
-  # My theme
+  ylim(0,6) +
   my_theme
 
 # Printing Plot
@@ -768,7 +876,7 @@ print(parallel_trend_plot)
 output_file_swedish <- file.path(path_output_swedish, "parallel_trend_plot.png")
 
 # Save the plot
-ggsave(output_file_swedish, parallel_trend_plot, bg = "transparent", width=5, height=4)
+ggsave(output_file_swedish, parallel_trend_plot, bg = "transparent", width=6, height=3)
 
 #
 #
@@ -811,6 +919,7 @@ dynamic_did_plot <- ggplot(data = dynamic_DiD_results_clustered,
   ylab('Average percent of students with F') +
   geom_hline(yintercept = 0, linetype = "solid", color = 'brown', alpha = 0.75) +  
   geom_vline(xintercept = -0.5, linetype = "dashed") +
+  ylim(-2,2) +
   my_theme
 
 # Print Dynamic DiD Plot
@@ -820,7 +929,7 @@ print(dynamic_did_plot)
 output_file_swedish <- file.path(path_output_swedish, "dynamic_did_plot.png")
 
 # Save the plot
-ggsave(output_file_swedish, dynamic_did_plot, bg = "transparent", width=5, height=4)
+ggsave(output_file_swedish, dynamic_did_plot, bg = "transparent", width=6, height=3)
 
 
 #
